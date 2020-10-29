4d54d2f41b6051b8b0a3810ec9a30884c40caea9
Amanda Marcal <amandabmarcal@gmail.com>
2020-10-28 23:45:17 -0400

    Update MostFrequentWeekdays.js

--- MostFrequentWeekdays.js
+++ MostFrequentWeekdays.js
@@ -1,4 +1,4 @@
-/**What is your favourite day of the week? Check if it's the most frequent day of the week in the year.
+/**What is your favorite day of the week? Check if it's the most frequent day of the week in the year.
 You are given a year as integer (e. g. 2001). 
 You should return the most frequent day(s) of the week in that year. 
 The result has to be a list of days sorted by the order of days in week (e. g. ['Monday', 'Tuesday'], ['Saturday', 'Sunday'], ['Monday', 'Sunday']). 
