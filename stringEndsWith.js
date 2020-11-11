f14853253342ee63396b3158f9daaaf02ebd16ac
Amanda Marcal <amandabmarcal@gmail.com>
2020-11-10 19:01:53 -0500

    Update stringEndsWith.js

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -1,3 +1,12 @@
+/* 
+Complete the solution so that it returns true if the first argument(string)
+ passed in ends with the 2nd argument (also a string).
+/* 
+Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
 // check if str == second str
 // str.substring
 
@@ -21,5 +30,8 @@ function solution(str, ending){
 const solution = (str, ending) => {
     return str.endsWith(ending)
 }
+
+
+
 
 
